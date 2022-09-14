01007343885ab6843e7ccb7e687c2db5050083c7
Hernán Bustamante Hesse <97261739+HernanBHesse@users.noreply.github.com>
2022-09-14 12:09:58 -0300

    Update ItemList.js

--- src/componentes/ItemList/ItemList.js
+++ src/componentes/ItemList/ItemList.js
@@ -24,7 +24,7 @@ const ItemList = ({ tituloSeccion, products }) => {
       <Grid
         container
         spacing={3}
-        sx={{ width: "80vw", placeContent: "center", mx: "auto" }}
+        sx={{ width: "80vw", placeContent: "center", mx: "auto", mt: 1 }}
       >
         {products.map(({ id, titulo, precio, imagen, altImagen, stock }) => {
           return (
